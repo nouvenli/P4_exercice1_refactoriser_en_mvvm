fd64fc425b6588f6df5005fbd300cf34c5316d61
jacqueline R <jacqueline.raynaud@gmail.com>
2025-11-05 19:19:42 +0100

    c5 - Moving the view files into presentation/view (and also the data)

---
+++ app/src/main/java/com/openclassrooms/notes/presentation/View/NoteItemDecoration.kt
@@ -0,0 +1,36 @@
+package com.openclassrooms.notes.presentation.View
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * A decoration that adds spacing between items in a RecyclerView.
+ * @param spaceSize The size of the spacing in pixels.
+ * @param spanCount The number of columns in the RecyclerView.
+ */
+class NoteItemDecoration(
+    private val spaceSize: Int,
+    private val spanCount: Int
+) : RecyclerView.ItemDecoration() {
+
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        with(outRect) {
+            if (parent.getChildAdapterPosition(view) < spanCount) {
+                top = spaceSize
+            }
+
+            if (parent.getChildAdapterPosition(view) % spanCount == 0) {
+                left = spaceSize
+            }
+
+            right = spaceSize
+            bottom = spaceSize
+        }
+    }
+}
---
+++ app/src/main/java/com/openclassrooms/notes/presentation/View/NotesAdapter.kt
@@ -0,0 +1,37 @@
+package com.openclassrooms.notes.presentation.View
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.openclassrooms.notes.Domain.MyNote
+import com.openclassrooms.notes.databinding.NoteBinding
+import com.openclassrooms.notes.presentation.View.NoteViewHolder
+
+/**
+ * An adapter for displaying a list of notes in a RecyclerView.
+ * @param notes The list of notes to display.
+ */
+class NotesAdapter(private var notes: List<MyNote>) : RecyclerView.Adapter<NoteViewHolder>() {
+
+    /**
+     * Updates the list of notes displayed by the adapter.
+     * @param newNotes The new list of notes to display.
+     */
+    fun updateNotes(newNotes: List<MyNote>) {
+        notes = newNotes
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val binding = NoteBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return NoteViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int =
+        notes.size
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val note = notes[position]
+        holder.bind(note)
+    }
+}
